e616fc692c5bd56a0a91dafc2a019a0fd38a2d7c
horshohei <61722727+horshohei@users.noreply.github.com>
2021-12-20 13:14:31 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,9 +3,7 @@ function App() {
     return (
         <Container>
             <Row>
-                <Col md={{ offset: 3, span: 6 }}>
-                    <h1>Todo App</h1>
-                </Col>
+            <Col md={{ offset: 3, span: 6 }}><h1>Todo App</h1></Col>
             </Row>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
